747c5cbb4bc249c18e2d8b46a7cbc41ff7807826
unknown <dhillonarmaan2004@gmail.com>
2023-01-07 01:16:19 +0530

    fetch and category setup

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,38 @@
+import styled from "styled-components";
+
 const About=()=>{
     return <>
+   <Wrapper>
 
-    about page
+       <div className='main'>
+           <div className='image-holder'>
+               <img src="" alt=""/>
+           </div>
+           <div>
+               <div>
+                   <h2>Our Story</h2>
+               </div>
+               <div>
+                   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur error est explicabo necessitatibus soluta. Architecto enim libero pariatur sed soluta!
+               </div>
+           </div>
+       </div>
+   </Wrapper>
+
     </>
 }
+
+
+const Wrapper=styled.div`
+  .image-holder{
+    width: 50%;
+  }
+ .main{
+   display: flex;
+   justify-content: space-evenly;
+   margin: 1.4rem;
+   background: blanchedalmond;
+ }
+`
 
 export default About
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,9 +1,139 @@
-const Products=()=>{
-    return <>
+import {useEffect, useState} from "react";
+import styled from "styled-components";
 
-    Products page
-    </>
+
+const Products=()=>{
+    const [loader,setloader]=useState(false)
+    const [data,setdata]=useState([])
+
+    // const fetchdata=async ()=>{
+    //     await fetch('https://course-api.com/react-store-products').then((res)=>{
+    //         res.json().then((data)=>{
+    //             setdata(data)
+    //             setloader(false)
+    //         })
+    //     })
+    //
+    // }
+    // useEffect(()=>{
+    //
+    //     fetchdata()
+    //     console.log(data)
+    //
+    // },[])
+
+    if(loader){
+        return<>
+        <h1>Loading</h1>
+        </>
+    }
+    else{
+        return <>
+            <Wrapper>
+                 <div className='main'>
+                     <div className='search'>
+                     Search
+                     </div>
+                     <div className='middle'>
+                         <div className='category'>
+                             <div className='category-cat'>
+                                 <div className='heading'>
+                                 <h2>Category</h2>
+                                 </div>
+                                 <div>
+
+                                 <button>All</button>
+                                 </div>
+                                 <div>
+
+                                 <button>Office</button>
+                                 </div>
+
+                                 <div>
+
+                                 <button>Living Room</button>
+                                 </div>
+
+                                 <div>
+
+                                 <button>Kitchen</button>
+                                 </div>
+
+                                 <div>
+
+                                 <button>Bedroom</button>
+                                 </div>
+
+                                 <div>
+
+                                 <button>Dining</button>
+                                 </div>
+                                 <div>
+
+                                 <button>Kids</button>
+                                 </div>
+                             </div>
+
+                             <div>
+                                 <h2>Company</h2>
+                             </div>
+
+                             <div>
+                                 <h2>Colors</h2>
+                             </div>
+
+                         </div>
+                         <div className='products'>
+                                Products
+                         </div>
+                     </div>
+                 </div>
+            </Wrapper>
+        </>
+    }
+
 }
+
+const Wrapper=styled.div`
+ .heading{
+   padding: 1.2rem;
+ }
+  button{
+    border: none;
+    background: aliceblue;
+    text-align: left;
+    margin: 0;
+    padding: 1.2rem;
+    font-size: 1.25rem;
+    
+  }
+  button:hover{
+    cursor: pointer;
+    color: #39A1AE;
+  }
+  .search{
+    background: aquamarine;
+  }
+.main{
+  display: flex;
+  flex-direction: column;
+ 
+  
+  margin: 1.4rem;
+  background: aliceblue;
+}
+  .middle{
+    display: flex;
+    justify-content: space-between;
+  }
+  .category{
+    width: 30%;
+  }
+  .products{
+    width: 95%;
+    background: blanchedalmond;
+  }
+`
 
 
 
